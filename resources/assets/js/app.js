d70c5c871199b0c6508a71c375a6af8e77132844
Cauê <nncl@live.com>
2015-11-18 09:51:32 -0200

    Adding frontend stuff

---
+++ resources/assets/js/app.js
@@ -0,0 +1,33 @@
+;(function( window, document, $, undefined ) {
+  'use strict';
+
+  var app = (function() {
+
+    var $private = {};
+    var $public = {};
+
+    $private.privateVar = 'private var';
+    $public.publicVar = 'public var';
+
+    $private.privateMethod = function() {
+      return 'Private method';
+    };
+
+    $public.publicMethod = function() {
+      return 'Public with ' + $private.privateMethod();
+    };
+
+    $public.responsiveMenu = function() {
+      $('#menu').slicknav({
+        prependTo:'.responsive-nav'
+      });
+    };
+
+    return $public;
+  })();
+
+  // Global
+  window.app = app;
+  app.responsiveMenu();
+
+})( window, document, jQuery );
